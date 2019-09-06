0b6aae2cf554623f42bdc827f326189b5a46275e
Iann Isacksson <iisackssonoliveira@gmail.com>
2019-09-06 09:40:19 -0300

    Tela inicial

---
+++ src/styles/colors.js
@@ -0,0 +1,14 @@
+export default {
+  white: '#FFF',
+  lighter: '#D2D8CF',
+  light: '#D5D5D5',
+  dark: '#464646',
+  black: '#000',
+
+  primary: '#00A9CF',
+  secundary: '#FFCF00',
+
+  transparent: 'transparent',
+  darkTransparent: 'rgba(0,0,0,0.6)',
+  whiteTransparent: 'rgba(255,255,255, 0.3)',
+};
